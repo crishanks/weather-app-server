03f3a8fbd14e9b4fd918db2c998314bb70a123ac
github username <crishanks12@gmail.com>
2019-08-23 20:46:46 -0600

    handlebars and hbs for dynamic pages

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 const path = require('path')
 const express = require('express')
 
+const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-const app = express()
-
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Cris'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Cris'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    helpText: 'Helpful text!'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({

8af878dc38d54eb5e04390059cb0a842bee741ff
github username <crishanks12@gmail.com>
2019-08-26 21:43:29 -0600

    created partials for header and footer. updated render in app.get for each page, pointed hbs to /templates routes for custom directory name

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setu phandlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set up static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -24,14 +34,17 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
-    helpText: 'Helpful text!'
+    helpText: 'Helpful text!',
+    title: 'Help',
+    name: 'Cris'
   })
 })
 
 app.get('/weather', (req, res) => {
   res.send({
     forcast: 'Snowing',
-    location: 'Utah'
+    location: 'Utah',
+    name: 'Cris'
   })
 })
 

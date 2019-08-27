46f94f6ef519b83ac02d80f4d0af0eec1a53738e
github username <crishanks12@gmail.com>
2019-08-27 15:56:14 -0600

    input and search rendering complete. form styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,27 @@
 console.log('Client side javascript file is loaded!')
 
-// fetch('http://puzzle.mead.io/puzzle')
-//   .then((response) => {
-//     return response.json()
-//     .then(data => {
-//       console.log(data)
-//     })
-//   })
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('message-1')
+const messageTwo = document.getElementById('message-2')
 
-fetch('http://localhost:3000/weather?address=!')
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
   .then(response => {
     return response.json()
   })
   .then(data => {
     if (data.error) {
-      console.log(data.error)
+      messageTwo.textContent = data.error
     } else {
-      console.log(data.location)
-      console.log(data.forecast)
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
     }
   })
+})
